cacf4d58e35090cf4ed6ea31f4f00200c573610b
Taisha Joseph <taishajo@mit.edu>
2025-09-22 11:30:28 -0400

    Add a smart search page

--- seek/urls.py
+++ seek/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     # re_path(r'^api/swagger/$', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     # re_path(r'^api/redoc/$', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
+    re_path(r'^salt/', views.smartSearch, name='smartSearch'),
     re_path(r'^remote/', views.remote, name='remote'),
     re_path(r'^samples/upload/', views.sampleUpload, name='sampleUpload'),
     re_path(r'^samples/query/', views.sampleQuery, name='sampleQuery'),
