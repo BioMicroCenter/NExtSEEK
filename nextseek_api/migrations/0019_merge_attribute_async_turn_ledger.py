57c178f4b6f9a75a80cf9b342e4192cd72dc3809
taishajo <taishajo@mit.edu>
2026-08-14 15:40:31 -0400

    fix(attributes): reconcile merged migration graph

---
+++ nextseek_api/migrations/0019_merge_attribute_async_turn_ledger.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("nextseek_api", "0011_attribute_async_orchestration"),
+        ("nextseek_api", "0018_turn_ledger_attempted_provenance"),
+    ]
+
+    operations = []
